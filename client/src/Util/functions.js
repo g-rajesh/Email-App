bb7fe7d00b98b23a5349ab1deaa39021438d655a
Sri Jayan <sjayan2002@gmail.com>
2021-10-17 18:32:51 +0530

    signin page functionality's error fixed

--- client/src/Util/functions.js
+++ client/src/Util/functions.js
@@ -4,33 +4,40 @@ const keys = QuickEncrypt.generate(1024);
 const publicKey = keys.public;
 const privateKey = keys.private;
 
-export const storeUserDetails = (checkBox,formDetails) => {
-    if(checkBox){
-        // let encryptedText = QuickEncrypt.encrypt( "This is some super top secret text!", publicKey )
+export const storeUserDetails = (checkBox, formDetails) => {
+  if (checkBox) {
+    // let encryptedText = QuickEncrypt.encrypt( "This is some super top secret text!", publicKey )
 
-        const firstName = QuickEncrypt.encrypt(formDetails.firstName, publicKey);
-        const lastName = QuickEncrypt.encrypt(formDetails.lastName, publicKey);
-        const email = QuickEncrypt.encrypt(formDetails.email, publicKey);
-        const password = QuickEncrypt.encrypt(formDetails.password, publicKey);
-        
-        localStorage.setItem('userDetails', JSON.stringify({
-            firstName, lastName, email, password
-        }));
-    }
-    else{
-        localStorage.setItem('userDetails', localStorage.getItem('userDetails'));
-    }
-}
+    const firstName = QuickEncrypt.encrypt(formDetails.firstName, publicKey);
+    const lastName = QuickEncrypt.encrypt(formDetails.lastName, publicKey);
+    const email = QuickEncrypt.encrypt(formDetails.email, publicKey);
+    const password = QuickEncrypt.encrypt(formDetails.password, publicKey);
 
-export const getUserDetails = ()=>{
-    const userDetails = JSON.parse(localStorage.getItem('userDetails'));
-
-    const email = QuickEncrypt.decrypt(userDetails.email, privateKey);
-    const password = QuickEncrypt.decrypt(userDetails.password, privateKey);
-
-    return {
+    localStorage.setItem(
+      "userDetails",
+      JSON.stringify({
+        firstName,
+        lastName,
         email,
-        password
-    }
-    
-}
+        password,
+      })
+    );
+  } else {
+    localStorage.setItem("userDetails", localStorage.getItem("userDetails"));
+  }
+};
+
+export const getUserDetails = () => {
+  const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+  let email = "";
+  let password = "";
+  if (userDetails) {
+    email = QuickEncrypt.decrypt(userDetails.email, privateKey);
+    password = QuickEncrypt.decrypt(userDetails.password, privateKey);
+  }
+
+  return {
+    email,
+    password,
+  };
+};
